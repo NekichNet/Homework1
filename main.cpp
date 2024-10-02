b26c908a3aa84f86299ab352ebb14500d536fb74
NikitaNet <102032304+NekichNet@users.noreply.github.com>
2024-10-02 15:06:58 +0500

    again fixed range

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
 
-	int m[20];
+	int m[20], is9;
 	
 	for (int i = 0; i < 20; i++) {
-		m[i] = std::rand() % 17 - 5;
+		m[i] = std::rand() % 16 - 5;
 		std::cout << m[i] << ' ';
 		if (i == 9) {
 			std::cout << '\n';
